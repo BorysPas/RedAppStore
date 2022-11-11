849bfd3bd8d81de7f7326f9a73592959d0861d90
Borys <BoreasPas@gmail.com>
2022-11-11 12:26:45 +0100

    Main menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,25 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        System.out.print("Hello World");
+        System.out.println("Punkt meni(1-10):");
+        int i = 0;
+        do {
+            i = getInt();
+            switch(i) {
+             case 1:
+
+                System.out.println("Punkt meni---" + i);
+                break;
+             case 0:
+                System.out.println("Game over");
+                break;
+             default:
+                System.out.println("Od 1 do 10!");}
+        }
+        while (i != 0);
+    }
+    public static int getInt() {
+        return new Scanner(System.in).nextInt();
     }
 }
