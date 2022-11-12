bceb1738ecdae0d15c1324b8f495bbb5c9b5f86f
Borys <BoreasPas@gmail.com>
2022-11-12 18:22:11 +0100

    Main menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,27 @@
 package com.company;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 public class Main {
     public static void main(String[] args){
-        System.out.println("Punkt meni(1-10):");
-        int i = 0;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEEE, MMMM d, yyyy");
+        Calendar calendar = new GregorianCalendar(2020, Calendar.JANUARY , 1);
+
+        calendar.add(Calendar.DATE, 4);
+        System.out.println(dateFormat.format(calendar.getTime()));
+      //  System.out.println(calendar.get((Calendar.DAY_OF_WEEK == 1) ? "weekend": "work");
+        System.out.println((calendar.get(Calendar.DAY_OF_WEEK) == 1)||(calendar.get(Calendar.DAY_OF_WEEK) == 7) ? "weekend" : "work");
+        System.out.println(calendar.get(Calendar.DAY_OF_WEEK));
+        System.out.println(weekendOrNot(calendar.get(Calendar.DAY_OF_WEEK)));
+
+ /*       System.out.println("Punkt meni(1-10):");
+        int i;
         do {
             i = getInt();
             switch(i) {
              case 1:
-
                 System.out.println("Punkt meni---" + i);
-                break;
              case 0:
                 System.out.println("Game over");
                 break;
@@ -18,6 +29,9 @@ public class Main {
                 System.out.println("Od 1 do 10!");}
         }
         while (i != 0);
+ */   }
+    public static boolean weekendOrNot(int arg){
+        return ((arg == 1) || (arg == 7) ? true : false);
     }
     public static int getInt() {
         return new Scanner(System.in).nextInt();
